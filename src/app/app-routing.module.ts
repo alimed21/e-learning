b2d3868977c3411ffd8493d13f5f81122e58c5b9
alimohamed <ali.mohamed@ansie.dj>
2019-02-03 09:29:51 +0300

    Porjet e-learning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AproposComponent } from './apropos/apropos.component';
+import { CoursesComponent } from './courses/courses.component';
+import { EvenementComponent } from './evenement/evenement.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', redirectTo: '/accueil', pathMatch: 'full' },
+  {path:'accueil', component: AccueilComponent},
+  {path:'apropos', component: AproposComponent},
+  {path:'courses', component: CoursesComponent},
+  {path:'evenement', component: EvenementComponent},
+  {path:'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AproposComponent } from './apropos/apropos.component';
+import { CoursesComponent } from './courses/courses.component';
+import { EvenementComponent } from './evenement/evenement.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    AproposComponent,
+    CoursesComponent,
+    EvenementComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
